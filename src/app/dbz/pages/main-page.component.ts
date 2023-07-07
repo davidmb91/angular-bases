533b6c3016d7393be5f93307a28314a109205ffc
David Moreno Buj <davidmb91@gmail.com>
2023-07-07 17:25:40 +0200

    Primera versión uso de servicios

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Character } from '../interfaces/characters.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,27 +8,7 @@ import { Character } from '../interfaces/characters.interface';
 
 export class MainPageComponent {
 
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  },
-  {
-    name: 'Goku',
-    power: 9500
-  },
-  {
-    name: 'Vegeta',
-    power: 7500
-  }];
-
-  constructor() { }
-
-  onNewCharacter(character: Character): void {
-    this.characters.push(character);
-  }
-
-  onDeleteCharacter(id: number): void {
-    this.characters.splice(id, 1);
+  constructor(public dbzService: DbzService) {
   }
 
 }
